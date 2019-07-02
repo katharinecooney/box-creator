e401a587936027363fdff80724f2b41ed556315b
Katharine Cooney <katharinecooney@gmail.com>
2019-07-02 13:12:39 -0500

    Add logic in BoxList to display all boxes

--- src/BoxList.js
+++ src/BoxList.js
@@ -1,18 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Box from './Box';
 
 class BoxList extends Component {
   constructor(props){
     super(props);
     this.state = {
-      allBoxes: []
+      allBoxes: [
+        {height: 50, width: 100, color: 'purple'},
+        {height: 150, width: 300, color: 'magenta'},
+      ]
     }
   }
 
 
   render(){
+    const boxes = this.state.allBoxes.map(box => <Box height={box.height} width={box.width} color={box.color}/>);
     return(
       <div>
         <h1>Make a Box!</h1>
+        {boxes}
       </div>
     )
   }
